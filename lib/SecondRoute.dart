ee916544de084ea539ed50f83814274d585630a0
Andrew <stallingswandrew@gmail.com>
2021-05-10 13:59:17 -0700

    Footer added to building page

--- lib/SecondRoute.dart
+++ lib/SecondRoute.dart
@@ -8,14 +8,49 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Second Route"),
       ),
-      body: Center(
-        child: ElevatedButton(
+      body: Text("Text"),
+        floatingActionButton: FloatingActionButton (
+          child: Icon(Icons.home),
           onPressed: () {
             Navigator.pop(context);
-          },
-          child: Text('Go back!'),
+          }
         ),
+      bottomNavigationBar: BottomAppBar (
+        child: new Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            new Container(
+              child: new Text('Addresss'), //Text
+            ),
+            new Container(
+              child: new Text('Phone Number',
+                textAlign: TextAlign.justify),
+            ),
+          ],
+        ),
+      color: Colors.red,
       ),
     );
   }
 }
+
+// class SecondRoute extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("Second Route"),
+//       ),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             Navigator.pop(context);
+//           },
+//
+//           child: Text('Go back!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
