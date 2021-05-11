2125b919321b3578b59215781dcdaaa9cd9bc8ed
Jeremiah Smith <jeremiah@Jeremiahs-MacBook-Pro.local>
2021-05-11 08:02:22 -0700

    Carousel

--- ios/Runner.xcworkspace/config.dart
+++
@@ -1,3 +0,0 @@
-const Map properties = const {
-  'iOSAPIKey': 'AIzaSyCy1oq_MRmbIvDtUOdycL6DjyIcOr6zG4w'
-};
--- lib/SecondRoute.dart
+++ lib/SecondRoute.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:carousel_slider/carousel_slider.dart';
 
 class SecondRoute extends StatelessWidget {
   @override
@@ -8,7 +8,50 @@ class SecondRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Second Route"),
       ),
-      body: Text("Text"),
+      body: ListView(
+        children: [
+          CarouselSlider(
+            items: [
+              //1st Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage("images/img1.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+              //2nd Image of Slider
+              Container(
+                margin: EdgeInsets.all(6.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8.0),
+                  image: DecorationImage(
+                    image: AssetImage("images/img2.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+            ],
+
+            //Slider Container properties
+            options: CarouselOptions(
+              height: 500.0,
+              enlargeCenterPage: true,
+              autoPlay: false,
+              aspectRatio: 16 / 9,
+              autoPlayCurve: Curves.fastOutSlowIn,
+              enableInfiniteScroll: false,
+              autoPlayAnimationDuration: Duration(milliseconds: 800),
+              viewportFraction: 0.8,
+            ),
+          ),
+        ],
+      ),
         floatingActionButton: FloatingActionButton (
           child: Icon(Icons.home),
           onPressed: () {
