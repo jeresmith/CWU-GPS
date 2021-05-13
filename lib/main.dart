f0ade211e309d57b7a8aee7482471e2e0a35af51
zackareeperkins <82546982+zackareeperkins@users.noreply.github.com>
2021-05-13 13:28:06 -0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 
 import 'package:cwu_gps/CWUBuildingMarkers.dart';
 import 'package:flutter/material.dart';
+import 'CreateDrawer.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() => runApp(MyApp());
@@ -39,75 +40,7 @@ class _MyAppState extends State<MyApp> {
           mapType: MapType.hybrid,
         ),
         //Hamburger menu
-        drawer: Drawer(
-          child: ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              DrawerHeader(
-                child: Text('Buildings'),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-              ListTile(
-                title: Text('Surc'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Samuelson'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Library'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Sue Lombard'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Parking lot'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Black Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Barto Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Kamola Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Green Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Dugmore Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Wilson Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Quigley Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Sparks Hall'),
-                onTap: () {},
-              ),
-              ListTile(
-                title: Text('Meisner Hall'),
-                onTap: () {},
-              ),
-            ],
-          ),
-        ),
+        drawer: CreateDrawer(mapController),
         //Blue App bar at the top
         appBar: AppBar(
           leading: Builder(
